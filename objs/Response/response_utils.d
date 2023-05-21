a8c922c826ca811492f93806554300f2cc694979
kyamagis <rna36001@gmail.com>
2023-05-21 22:19:59 +0900

    fix:CGIのメソッドによる分岐を修正

--- objs/Response/response_utils.d
+++
@@ -1,7 +0,0 @@
-objs/Response/response_utils.o: srcs/Response/response_utils.cpp \
- srcs/Response/../../includes/response_utils.hpp \
- srcs/Response/../../includes/./utils.hpp
-
-srcs/Response/../../includes/response_utils.hpp:
-
-srcs/Response/../../includes/./utils.hpp:
--- objs/config/Location.d
+++
@@ -1,8 +0,0 @@
-objs/config/Location.o: srcs/config/Location.cpp includes/Location.hpp \
- includes/Value.hpp includes/utils.hpp
-
-includes/Location.hpp:
-
-includes/Value.hpp:
-
-includes/utils.hpp:
