76d7c2929fb469e21edd2ce4eb267d358f2c0224
kyamagis <rna36001@gmail.com>
2023-06-09 23:25:22 +0900

    fix: CGIのpyを修正

--- contents/cgi_files/GET.py
+++ contents/cgi_files/GET.py
@@ -2,7 +2,7 @@
 import sys
 
 sys.stdout.write('Content-type: text/css\r\n')
-sys.stdout.write('COntent-length: 25\r\n\r\n')
+sys.stdout.write('COntent-length: 25\r\n')
 sys.stdout.write('\r\n')
 sys.stdout.write('GET python\n')
-sys.stdout.write('天気晴朗なれども波高し\n')
+sys.stdout.write('May the Force be with you.')
